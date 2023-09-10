f88dd497cfb3372a3663a88bd84e602a993b292f
AhmedSamy52 <140157608+AhmedSamy52@users.noreply.github.com>
2023-09-10 05:24:07 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry Point
  *
- * Description: prints the size of various types 
+ * Description: prints the size of various types
  * on the computer it is compiled and run on.
  *
  * Return: Always 0 (success)
